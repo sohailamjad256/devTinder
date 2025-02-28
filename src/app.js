c581e5b52afa8b178596af29f4379d526c951293
sohailamjad256 <sohailamjad20mar1998@gmail.com>
2025-03-01 01:55:23 +0530

    exploring http methods

--- src/app.js
+++ src/app.js
@@ -2,8 +2,31 @@ const express =  require("express")
 
 const app = express()
 
-app.use((req,res)=>{
-    res.send("server started")
+// app.use('/users', (req,res)=>{
+//     res.send("Hahaha")
+// })
+
+app.get('/users',(req,res)=>{
+    res.send({firstname:"Sohail", lastname:"amjad"})
 })
+
+app.delete('/users', (req,res)=>{
+    res.send('deleted Data')
+})
+
+app.patch('/users',(req,res)=>{
+    res.send('data patched')
+})
+// This will only match with only particular http method
+app.post('/users',(req,res)=>{
+    res.send("data saved successfully")
+})
+//this will match all th HTTP method Api calls to /hello
+app.use("/hello", (req,res)=>{
+    res.send("Hello 3000")
+})
+
+
+
 
 app.listen(3000)
