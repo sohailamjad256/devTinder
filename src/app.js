a400cedcb3916da1d826542013b04ee8956f9ad1
sohailamjad256 <sohailamjad20mar1998@gmail.com>
2025-03-02 01:32:13 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,29 +2,69 @@ const express =  require("express")
 
 const app = express()
 
+
+
+app.use('/users',(req,res,next)=>{
+    console.log('response from server');
+    
+    next()
+    //res.send('from Server1')
+},
+(req,res,next)=>{
+    console.log('response from server 2');
+
+  //  res.send('from server 2')
+    next()
+    
+},
+(req,res,next)=>{
+    console.log('response from server 3');
+  //  res.send('from server 3')
+    next()
+    
+},
+(req,res,next)=>{
+    console.log('response from server 4');
+    res.send('from server 4')
+    next()
+    
+}
+
+);
+
 // app.use('/users', (req,res)=>{
 //     res.send("Hahaha")
 // })
 
-app.get('/users',(req,res)=>{
-    res.send({firstname:"Sohail", lastname:"amjad"})
-})
 
-app.delete('/users', (req,res)=>{
-    res.send('deleted Data')
-})
+// app.get('/users', (req, res)=>{
+//     console.log(req.query);
+    
+//     res.send({firstname:'sohail'})
+// })
 
-app.patch('/users',(req,res)=>{
-    res.send('data patched')
-})
-// This will only match with only particular http method
-app.post('/users',(req,res)=>{
-    res.send("data saved successfully")
-})
-//this will match all th HTTP method Api calls to /hello
-app.use("/hello", (req,res)=>{
-    res.send("Hello 3000")
-})
+
+// app.get('/users/:userid/:name/:password',(req,res)=>{
+//     console.log(req.params);
+    
+//     res.send({firstname:"Sohail", lastname:"amjad"})
+// })
+
+// app.delete('/users', (req,res)=>{
+//     res.send('deleted Data')
+// })
+
+// app.patch('/users',(req,res)=>{
+//     res.send('data patched')
+// })
+// // This will only match with only particular http method
+// app.post('/users',(req,res)=>{
+//     res.send("data saved successfully")
+// })
+// //this will match all th HTTP method Api calls to /hello
+// app.use("/hello", (req,res)=>{
+//     res.send("Hello 3000")
+// })
 
 
 
